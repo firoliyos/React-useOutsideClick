d687d0b50e94fab7888bc31b85eb8b9e57a4e188
firoliyos <firatesfaye44@gmail.com>
2024-11-04 16:56:45 +0300

    end!

--- use-outside-click/src/components/test.jsx
+++ use-outside-click/src/components/test.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
+import useOutsideClick from '.'
 
 export default function UseOnclickOutsideTest() {
     const [showContent, setShowContent] = useState(false)
+    const ref = useRef()
+    useOutsideClick(ref, () => setShowContent(false))
     return(
         <div>
          {
-            showContent ? <div>
+            showContent ? <div ref={ref}>
                 <h1>This is a random content</h1>
                 <p>Please click outside of this to close. it won't close if you click inside this content!</p>
             </div> 
